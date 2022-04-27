aaa5540b4bacd3e05e4cdf5b7eec74b66128248d
matthewstavig7 <matthewstavig01@gmail.com>
2022-04-27 12:48:33 -0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,26 @@
 #include <SFML/Graphics.hpp>
 #include "Minesweeper.h"
+#include "Test.h"
 
 int main()
 {
 	Minesweeper app;
+	Test test;
+	
+	// test cases
+	cout << "5 Test Cases:" << endl;
+	test.testGetType();
+	test.testBoardConstructor();
+	test.testgameOver();
+	test.testgameWon();
+	test.testBorderBombCount();
+
+	system("pause");
+	system("cls");
+
+	// application
+	cout << "Use arrow keys and press enter to select menu options" << endl;
 	app.run();
+
 	return 0;
 }
